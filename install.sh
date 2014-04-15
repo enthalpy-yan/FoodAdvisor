bc03c73c616e9a27cf219ebb5a9f7fac88083222
enthalpy-yan <enthalpy.yan@gmail.com>
2014-04-15 19:31:28 -0400

    add flask restful module

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ flask/bin/pip install setuptools --no-use-wheel --upgrade
 flask/bin/pip install flask
 flask/bin/pip install flask-login
 flask/bin/pip install flask-openid
+flask/bin/pip install flask-restful
 flask/bin/pip install flask-mail==0.7.6
 flask/bin/pip install Flask-PyMongo
 flask/bin/pip install flask-wtf==0.8.4

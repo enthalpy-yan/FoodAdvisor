f0595cac483f03a185b4f7fc02d52b55f352965b
lastforeverzl <lastforeverzl@gmail.com>
2014-04-17 19:02:14 -0400

    rename filename in uuid4

--- app/apphelpers/upload.py
+++ app/apphelpers/upload.py
@@ -4,3 +4,7 @@ from app import app
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+def generate_new_filename(file):
+    f, ext = os.path.splitext(file.filename)
+    return str(uuid.uuid4().hex) + ext

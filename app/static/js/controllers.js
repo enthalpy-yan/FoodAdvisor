ba387d22ebb35704f0ea01e38136d8bfda3656a1
enthalpy-yan <enthalpy.yan@gmail.com>
2014-04-18 09:36:21 -0400

    add food informations layout

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -5,9 +5,21 @@
 angular.module('foodAdvisor.controllers', []).
   controller('AppCtrl', function ($scope) {
   }).
-  controller('searchBarController', function ($scope) {
+  controller('searchBarController', function ($scope, $http) {
     $scope.currentValue = '';
+    $scope.imageData = null;
     $scope.submitText = function() {
         console.log($scope.keywords);
     };
+    $scope.getData = function() {
+        $http({method: 'GET', url: '/test'}).
+        success(function(data, status, headers, config) {
+            console.log(data)
+            $scope.imageData = data
+        }).
+        error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+        });
+    }
   });

7b3eb594d5053be54c62834be769506492e5a581
enthalpy-yan <enthalpy.yan@gmail.com>
2014-04-19 00:24:15 -0400

    fixed import error for update_collection

--- app/dbhelpers/preparedata.py
+++ app/dbhelpers/preparedata.py
@@ -5,6 +5,7 @@ Script for inserting data into database.
 """
 
 from inserthelper import insert_images
+from updatehelper import update_collection
 
 if __name__ == '__main__':
     insert_images('../outputs/images_data.txt', 'FoodAdvisor', 'images')

bef8b5f008a9c2041af1ea3701d2cef25ee2ecdb
lastforeverzl <lastforeverzl@gmail.com>
2014-04-19 11:38:01 -0400

    script for create full text index for DB

--- app/dbhelpers/preparedata.py
+++ app/dbhelpers/preparedata.py
@@ -5,8 +5,9 @@ Script for inserting data into database.
 """
 
 from inserthelper import insert_images
-from updatehelper import update_collection
+from updatehelper import update_collection, create_indexes
 
 if __name__ == '__main__':
     insert_images('../outputs/images_data.txt', 'FoodAdvisor', 'images')
     update_collection('FoodAdvisor', 'images')
+    create_indexes('test', 'images')

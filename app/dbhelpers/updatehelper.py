82cb055ef8db747b6e9be1a93b228f6a0e6b733a
lastforeverzl <lastforeverzl@gmail.com>
2014-04-19 00:04:05 -0400

    update db

---
+++ app/dbhelpers/updatehelper.py
@@ -0,0 +1,17 @@
+import json
+from pymongo import MongoClient
+
+def update_collection(db_name, collection_name):
+    client = MongoClient('localhost', 27017)
+    db = client[db_name]
+    collection = db[collection_name]
+    cursor = collection.find()
+    arr = []
+    for item in cursor:
+        image_id = item['_id']
+        description = item['description'].split()
+        collection.update(
+            {'_id': image_id}, 
+            {'$set': {'description_search': description}},
+            upsert=False
+        )

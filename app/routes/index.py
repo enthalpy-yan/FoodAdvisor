65915dc1528b3c2c290eba532ec97a44cb32d052
enthalpy-yan <enthalpy.yan@gmail.com>
2014-05-05 12:00:22 -0400

    disable request loggings

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,6 +1,10 @@
 from app import app
 from flask import send_file
 
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serveIndex(path):
